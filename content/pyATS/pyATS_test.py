78c91fb3b274620d9bb361ddc89f33a0ba3dd14a
chazorino <f.wagner@live.de>
2023-10-04 18:52:54 +0200

    pyATS edit

--- content/pyATS/pyATS_test.py
+++ content/pyATS/pyATS_test.py
@@ -1,9 +1,17 @@
 from pyats.topology.loader import load
 import sys
 
+# This script takes two arguments
+# 1: dev or prod for the device you want to connect to
+# 2: Number 1-8 for the interface GigabitEthernet 1/0/1-8 interfaces on the devices
+# Example: 
+# python3 pyATS_test.py dev 1 
+# For Interface GigabitEthernet 1/0/1 on the dev C9k Device
+
 tb = load('testbed.yaml')
 try:
-    interface_no = sys.argv[1]
+    device = sys.argv[1]
+    interface_no = sys.argv[2]
 except Exception as e:
     print("Parsing of Interface Config failed! Did you specify the Interface Number (0-8)?")
     print(e)
@@ -11,7 +19,7 @@ except Exception as e:
 
 interface = f"GigabitEthernet 1/0/{interface_no}"
 phrase = "I'm an automated description"
-dev = tb.devices['dev']
+dev = tb.devices[device]
 
 try:
     dev.connect(mit=True)
@@ -23,4 +31,4 @@ except Exception as e:
 if(phrase not in description):
     sys.exit()
 else:
-    print("Interface description test successful: ")
+    print(f"Interface description test successful: {phrase}")
